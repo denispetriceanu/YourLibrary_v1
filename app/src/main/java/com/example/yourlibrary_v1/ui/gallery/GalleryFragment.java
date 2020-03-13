098bcc9ad52d9d4eff9c2485091e72839c7920d7
alinpricop <alinpricop22@gmail.com>
2020-03-13 22:46:57 +0200

    Login

--- app/src/main/java/com/example/yourlibrary_v1/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/yourlibrary_v1/ui/gallery/GalleryFragment.java
@@ -1,27 +1,32 @@
 package com.example.yourlibrary_v1.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.yourlibrary_v1.R;
 
 public class GalleryFragment extends Fragment {
-
+    private Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        textView.setText("Gallery");
+        button= (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(),Login.class);
+                startActivity(in);
+            }
+        });
+
         return root;
     }
 }
