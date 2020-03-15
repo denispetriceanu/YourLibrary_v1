0991408cb117a3e69547796c03efa21ba8e077a4
alinpricop <alinpricop22@gmail.com>
2020-03-15 17:50:20 +0200

    Login + register

--- app/src/main/java/com/example/yourlibrary_v1/Forgotpass.java
+++ app/src/main/java/com/example/yourlibrary_v1/Forgotpass.java
@@ -13,86 +13,112 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Forgotpass extends AppCompatActivity implements OnClickListener {
-    private static View view;
+    private View view;
 
-    private static EditText emailId;
-    private static TextView submit, back;
+    private EditText emailId;
+    private TextView submit, back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpass);
+        Bundle bundle = getIntent().getExtras();
+        emailId = findViewById(R.id.login_emailid);
+        submit = findViewById(R.id.forgot_button);
+        back = findViewById(R.id.backToLoginBtn);
+        XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
+
+        {
+            ColorStateList csl = null;
+            try {
+                csl = ColorStateList.createFromXml(getResources(),
+                        xrp);
+            } catch (IOException | XmlPullParserException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("Test");
     }
 
     private void initViews() {
         emailId = view.findViewById(R.id.registered_emailid);
         submit = view.findViewById(R.id.forgot_button);
         back = view.findViewById(R.id.backToLoginBtn);
-        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
-        try {
-            ColorStateList csl = ColorStateList.createFromXml(getResources(),
-                    xrp);
-
-            back.setTextColor(csl);
-            submit.setTextColor(csl);
-
-        } catch (Exception e) {
-        }
-
-    }
-
-    // Set Listeners over buttons
-    private void setListeners() {
-        back.setOnClickListener(this);
-        submit.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.backToLoginBtn:
-
-                // Replace Login Fragment on Back Presses
-                new MainActivity();
-                break;
-
-            case R.id.forgot_button:
-
-                // Call Submit button task
-                submitButtonTask();
-                break;
-
-        }
-
-    }
-
-    private void submitButtonTask() {
-        String getEmailId = emailId.getText().toString();
-
-        // Pattern for email id validation
-        Pattern p = Pattern.compile(Utils.regEx);
-
-        // Match the pattern
-        Matcher m = p.matcher(getEmailId);
-
-        // First check if email id is not null else show error toast
-        if (getEmailId.equals("") || getEmailId.length() == 0)
-
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "Please enter your Email Id.");
-
-            // Check if email id is valid or not
-        else if (!m.find())
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "Your Email Id is Invalid.");
-
-            // Else submit email id and fetch passwod or do your stuff
-        else
-            Toast.makeText(new Activity(), "Get Forgot Password.",
-                    Toast.LENGTH_SHORT).show();
     }
 }
+//        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
+//        try {
+//            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+//                    xrp);
+//
+//            back.setTextColor(csl);
+//            submit.setTextColor(csl);
+//
+//        } catch (Exception e) {
+//        }
+//
+//    }
+
+    // Set Listeners over buttons
+//    private void setListeners() {
+//        back.setOnClickListener(this);
+//        submit.setOnClickListener(this);
+//    }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.backToLoginBtn:
+//
+//                // Replace Login Fragment on Back Presses
+//                new MainActivity();
+//                break;
+//
+//            case R.id.forgot_button:
+//
+//                // Call Submit button task
+//                submitButtonTask();
+//                break;
+//
+//        }
+//
+//    }
+//
+//    private void submitButtonTask() {
+//        String getEmailId = emailId.getText().toString();
+//
+//        // Pattern for email id validation
+//        Pattern p = Pattern.compile(Utils.regEx);
+//
+//        // Match the pattern
+//        Matcher m = p.matcher(getEmailId);
+//
+//        // First check if email id is not null else show error toast
+//        if (getEmailId.equals("") || getEmailId.length() == 0)
+//
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "Please enter your Email Id.");
+//
+//            // Check if email id is valid or not
+//        else if (!m.find())
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "Your Email Id is Invalid.");
+//
+//            // Else submit email id and fetch passwod or do your stuff
+//        else
+//            Toast.makeText(new Activity(), "Get Forgot Password.",
+//                    Toast.LENGTH_SHORT).show();
+//    }
+//}
--- app/src/main/java/com/example/yourlibrary_v1/SignUp.java
+++ app/src/main/java/com/example/yourlibrary_v1/SignUp.java
@@ -1,5 +1,4 @@
 package com.example.yourlibrary_v1;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.ColorStateList;
@@ -14,24 +13,49 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
-    private static View view;
-    private static EditText fullName, emailId, mobileNumber, location,
+    private  View view;
+    private  EditText fullName, emailId, mobileNumber, location,
             password, confirmPassword;
-    private static TextView login;
-    private static Button signUpButton;
-    private static CheckBox terms_conditions;
+    private TextView login;
+    private  Button signUpButton;
+    private  CheckBox terms_conditions;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Bundle bundle = getIntent().getExtras();
+        login = findViewById(R.id.loginBtn);
+        signUpButton = findViewById(R.id.signUpBtn);
+        terms_conditions = findViewById(R.id.terms_conditions);
+        XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
+
+        {
+            ColorStateList csl = null;
+            try {
+                csl = ColorStateList.createFromXml(getResources(),
+                        xrp);
+            } catch (IOException | XmlPullParserException ex) {
+                ex.printStackTrace();
+            }
+
+        }
     }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("Test");
+    }
+
 
     private void initViews() {
         fullName = view.findViewById(R.id.fullName);
@@ -43,88 +67,91 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         signUpButton = view.findViewById(R.id.signUpBtn);
         login = view.findViewById(R.id.already_user);
         terms_conditions = view.findViewById(R.id.terms_conditions);
-
-        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
-        try {
-            ColorStateList csl = ColorStateList.createFromXml(getResources(),
-                    xrp);
-
-            login.setTextColor(csl);
-            terms_conditions.setTextColor(csl);
-        } catch (Exception e) {
-        }
-    }
-
-    private void setListeners() {
-        signUpButton.setOnClickListener(this);
-        login.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.signUpBtn:
-
-                // Call checkValidation method
-                checkValidation();
-                break;
-
-            case R.id.already_user:
-
-                // Replace login fragment
-                new MainActivity();
-                break;
-        }
-
-    }
-
-    // Check Validation Method
-    private void checkValidation() {
-
-        // Get all edittext texts
-        String getFullName = fullName.getText().toString();
-        String getEmailId = emailId.getText().toString();
-        String getMobileNumber = mobileNumber.getText().toString();
-        String getLocation = location.getText().toString();
-        String getPassword = password.getText().toString();
-        String getConfirmPassword = confirmPassword.getText().toString();
-
-        // Pattern match for email id
-        Pattern p = Pattern.compile(Utils.regEx);
-        Matcher m = p.matcher(getEmailId);
-
-        // Check if all strings are null or not
-        if (getFullName.equals("") || getFullName.length() == 0
-                || getEmailId.equals("") || getEmailId.length() == 0
-                || getMobileNumber.equals("") || getMobileNumber.length() == 0
-                || getLocation.equals("") || getLocation.length() == 0
-                || getPassword.equals("") || getPassword.length() == 0
-                || getConfirmPassword.equals("")
-                || getConfirmPassword.length() == 0)
-
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "All fields are required.");
-
-            // Check if email id valid or not
-        else if (!m.find())
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "Your Email Id is Invalid.");
-
-            // Check if both password should be equal
-        else if (!getConfirmPassword.equals(getPassword))
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "Both password doesn't match.");
-
-            // Make sure user should check Terms and Conditions checkbox
-        else if (!terms_conditions.isChecked())
-            new CustomToast().Show_Toast(new Activity(), view,
-                    "Please select Terms and Conditions.");
-
-            // Else do signup or do your stuff
-        else
-            Toast.makeText(new Activity(), "Do SignUp.", Toast.LENGTH_SHORT)
-                    .show();
-
     }
 }
+
+//        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.xml.text_selector);
+//        try {
+//            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+//                    xrp);
+//
+//            login.setTextColor(csl);
+//            terms_conditions.setTextColor(csl);
+//        } catch (Exception e) {
+//        }
+//    }
+//
+//    private void setListeners() {
+//        signUpButton.setOnClickListener(this);
+//        login.setOnClickListener(this);
+//    }
+//}
+
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.signUpBtn:
+//
+//                // Call checkValidation method
+//                checkValidation();
+//                break;
+//
+//            case R.id.already_user:
+//
+//                // Replace login fragment
+//                new MainActivity();
+//                break;
+//        }
+//
+//    }
+//
+//    // Check Validation Method
+//    private void checkValidation() {
+//
+//        // Get all edittext texts
+//        String getFullName = fullName.getText().toString();
+//        String getEmailId = emailId.getText().toString();
+//        String getMobileNumber = mobileNumber.getText().toString();
+//        String getLocation = location.getText().toString();
+//        String getPassword = password.getText().toString();
+//        String getConfirmPassword = confirmPassword.getText().toString();
+//
+//        // Pattern match for email id
+//        Pattern p = Pattern.compile(Utils.regEx);
+//        Matcher m = p.matcher(getEmailId);
+//
+//        // Check if all strings are null or not
+//        if (getFullName.equals("") || getFullName.length() == 0
+//                || getEmailId.equals("") || getEmailId.length() == 0
+//                || getMobileNumber.equals("") || getMobileNumber.length() == 0
+//                || getLocation.equals("") || getLocation.length() == 0
+//                || getPassword.equals("") || getPassword.length() == 0
+//                || getConfirmPassword.equals("")
+//                || getConfirmPassword.length() == 0)
+//
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "All fields are required.");
+//
+//            // Check if email id valid or not
+//        else if (!m.find())
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "Your Email Id is Invalid.");
+//
+//            // Check if both password should be equal
+//        else if (!getConfirmPassword.equals(getPassword))
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "Both password doesn't match.");
+//
+//            // Make sure user should check Terms and Conditions checkbox
+//        else if (!terms_conditions.isChecked())
+//            new CustomToast().Show_Toast(new Activity(), view,
+//                    "Please select Terms and Conditions.");
+//
+//            // Else do signup or do your stuff
+//        else
+//            Toast.makeText(new Activity(), "Do SignUp.", Toast.LENGTH_SHORT)
+//                    .show();
+//
+//    }
+//}
