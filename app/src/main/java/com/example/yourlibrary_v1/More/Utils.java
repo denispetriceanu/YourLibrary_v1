ca419349d28b9843af034497e23a69c95f873c19
alinpricop <alinpricop22@gmail.com>
2020-03-19 22:05:18 +0200

    Detail

--- app/src/main/java/com/example/yourlibrary_v1/More/Utils.java
+++ app/src/main/java/com/example/yourlibrary_v1/More/Utils.java
@@ -3,7 +3,9 @@ package com.example.yourlibrary_v1.More;
 import android.content.Context;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseUser;
+
+
+import com.google.firebase.auth.FireabaseUser;
 
 public class Utils {
     public static final String regEx = "^[A-Za-z0-9+_.-]+@(.+)$";
@@ -11,7 +13,7 @@ public class Utils {
     public static final String signUp_activity = "SignUp_Activity";
     public static final String forgotPassword_activity = "ForgotPassword_Activity";
 
-    public void updateUI(FirebaseUser currentUser, Context context) {
+    public void updateUI(FirebaseDatabase currentUser, Context context) {
         if (currentUser == null) {
             Toast.makeText(context, "Nu esti conectat", Toast.LENGTH_LONG).show();
         } else {
