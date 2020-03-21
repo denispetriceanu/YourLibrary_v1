e54df353e801ddc3e546dadcf489539ca0b57bd2
Petriceanu Denis <49166081+denispetriceanu@users.noreply.github.com>
2020-03-21 20:27:23 +0200

    Push for Alin

--- app/src/main/java/com/example/yourlibrary_v1/Book_Details.java
+++ app/src/main/java/com/example/yourlibrary_v1/Book_Details.java
@@ -1,35 +1,19 @@
 package com.example.yourlibrary_v1;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.net.Uri;
-import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
-import android.renderscript.Sampler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.disklrucache.DiskLruCache;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
-
-import static com.example.yourlibrary_v1.ui.home.Adapters.HomeRecyclerViewAdapter.addChar;
 
 public class Book_Details extends AppCompatActivity {
     @Override
@@ -37,13 +21,14 @@ public class Book_Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book__details);
 
-        final TextView Title= findViewById(R.id.book_title_id);
-        final TextView author= findViewById(R.id.book_author_id);
-        final TextView category= findViewById(R.id.book_category_id);
+        final TextView Title = findViewById(R.id.book_title_id);
+        final TextView author = findViewById(R.id.book_author_id);
+        final TextView category = findViewById(R.id.book_category_id);
         final ImageView image = findViewById(R.id.book_img_id);
-        final TextView description= findViewById(R.id.book_description_id);
-        final TextView release= findViewById(R.id.book_releas_id);
-          // this line of code set the title for new activity
+        final TextView description = findViewById(R.id.book_description_id);
+        final TextView release = findViewById(R.id.book_releas_id);
+        final TextView rating = findViewById(R.id.book_rating_id);
+        // this line of code set the title for new activity
         Objects.requireNonNull(getSupportActionBar()).setTitle("Book details");
 
         final String book_id = getIntent().getStringExtra("book_id");
@@ -57,12 +42,21 @@ public class Book_Details extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 Book book = dataSnapshot.getValue(Book.class);
+
+                // ToDo: Trebuie facuta verificare la fiecare data de genul: if(title != "")...else(txtView.setTitle("Undefined")
+
                 author.setText(book.getAuthor());
                 Title.setText(book.getTitle());
 //                Picasso.with(this).load(Uri.parse(book.getLargeCoverUrl())).error(R.id.book_img_id).into(image);
-                category.setText(book.getCategory());
+                // rezolvam ca in cazul ratingului, vezi mai jos
+                category.setText(dataSnapshot.child("categories").getValue().toString());
                 description.setText(book.getDescription());
-                release.setText(book.getData_publisher());
+                release.setText(dataSnapshot.child("date_publisher").getValue().toString());
+                // nu stiu de ce nu prelua din book (probabil nu converteste bine datele, preluam direct
+                // prin punerea (int) in fata facem cast (adica convertim) din long in String (facem asta deoarece TextView-ul nu
+                // permite decat int sau String
+                String rating_converted = "Rating: " + dataSnapshot.child("rating").getValue();
+                rating.setText(rating_converted);
             }
 
             @Override
